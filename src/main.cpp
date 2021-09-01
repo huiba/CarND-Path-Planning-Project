2685be02d661d0b3c354184c3382179ed1006b8e
huiba <huiba@users.noreply.github.com>
2021-09-02 01:10:07 +0200

    basic planing working

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,57 @@
 #include "helpers.h"
 #include "json.hpp"
 #include "spline.h"
+#include "state.h"
 
 // for convenience
 using nlohmann::json;
 using std::string;
 using std::vector;
 
-double get_certer_s_from_lane(int lane)
+double get_certer_d_from_lane(int lane)
 {
   return (double)2 + 4 * lane; 
 }
 
+int get_lane_from_d(double d)
+{
+  return floor(d / 4);
+}
 
+double mph2mps(double mph)
+{
+  return mph * 0.44704; 
+}
+
+double mps2mph(double mps)
+{
+  return mps * 2.23694;
+}
+
+double min_cost_index(double a, double b, double c)
+{
+  double m = a;
+  int idx = 0;
+  if (b < m) {
+    m = b;
+    idx = 1;
+  }
+  if (c < m) {
+    m = c;
+    idx = 2;
+  }
+  return idx;
+}
+/* 
+vector<int> get_next(int state) 
+{
+
+}
+
+double cost(vector<double> predicted_state, vector<vector<double>> predictions)
+{
+}
+*/
 
 int main() {
   uWS::Hub h;
@@ -60,6 +99,12 @@ int main() {
 
   int lane = 1;
   double ref_vel = 0.0; // mph
+  // state: 0: keep lane, 1: change left lane, 2: change right lane
+  int state = 0;
+
+  const double SPEED_LIMIT = 49.5; //mph
+
+
 
   h.onMessage([&lane, &ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -79,7 +124,7 @@ int main() {
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -118,33 +163,145 @@ int main() {
           if (prev_size > 0) {
             car_s = end_path_s;
           }
-          bool too_close = false;
+          int our_lane = get_lane_from_d(car_d);
+          // car info: (s, d, vx, vy, speed, dist)
+          double max_dist = std::numeric_limits<double>::max();
+          vector<double> front_car = {-1, -1, -1, -1, -1, max_dist}; 
+          vector<double> left_front_car = {-1, -1, -1, -1, -1, max_dist};
+          vector<double> left_back_car = {-1, -1, -1, -1, -1, max_dist};
+          vector<double> right_front_car = {-1, -1, -1, -1, -1, max_dist};
+          vector<double> right_back_car = {-1, -1, -1, -1, -1, max_dist};
+
+          // find most relevant cars
+          double min_front_dist = std::numeric_limits<double>::max();
+          double min_left_front_dist = std::numeric_limits<double>::max();
+          double min_left_back_dist = std::numeric_limits<double>::max();
+          double min_right_front_dist = std::numeric_limits<double>::max();
+          double min_right_back_dist = std::numeric_limits<double>::max();
 
           for (int i = 0; i < sensor_fusion.size(); ++i) {
-            float d = sensor_fusion[i][6];
-            auto current_lane_center_s = get_certer_s_from_lane(lane);
-            if (d < current_lane_center_s + 2 && d > current_lane_center_s - 2) {
-              double vx = sensor_fusion[i][3];
-              double vy = sensor_fusion[i][4];
-              double check_speed = sqrt(vx * vx + vy * vy);
-              double check_car_s = sensor_fusion[i][5];
+            double f_s = sensor_fusion[i][5];
+            double f_d = sensor_fusion[i][6];
+            double f_vx = sensor_fusion[i][3];
+            double f_vy = sensor_fusion[i][4];
+            double f_speed = sqrt(f_vx * f_vx + f_vy * f_vy);
+            f_s += (double) prev_size * 0.02 * f_speed;
+            int f_lane = get_lane_from_d(f_d);
 
-              check_car_s += (double) prev_size * 0.02 * check_speed;
-              if (check_car_s > car_s && check_car_s - car_s < 30) {
-                //ref_vel = 29.5;
-                too_close = true;
-                if (lane > 0) {
-                  lane = 0;
+            double dist = car_s - f_s;
+            double abs_dist = std::abs(dist);
+            //1. same lane
+            if (our_lane == f_lane && dist <= 0) {
+              if (abs_dist < min_front_dist) {
+                min_front_dist = abs_dist;
+                front_car[0] = f_s;
+                front_car[1] = f_d;
+                front_car[2] = f_vx;
+                front_car[3] = f_vy;
+                front_car[4] = f_speed;
+                front_car[5] = abs_dist;
+              }
+            } 
+            //2. left lane
+            else if (our_lane - f_lane == 1) {
+              if (dist <= 0) {
+                // front
+                if (abs_dist < min_left_front_dist) {
+                  min_left_front_dist = abs_dist;
+                  left_front_car[0] = f_s;
+                  left_front_car[1] = f_d;
+                  left_front_car[2] = f_vx;
+                  left_front_car[3] = f_vy;
+                  left_front_car[4] = f_speed;
+                  left_front_car[5] = abs_dist;
+                }
+              } else {
+                // back
+                if (abs_dist < min_left_back_dist) {
+                  min_left_back_dist = abs_dist;
+                  left_back_car[0] = f_s;
+                  left_back_car[1] = f_d;
+                  left_back_car[2] = f_vx;
+                  left_back_car[3] = f_vy;
+                  left_back_car[4] = f_speed;
+                  left_back_car[5] = abs_dist;
                 }
               }
+            } 
+            //3. right lane
+            else if (our_lane - f_lane == -1) {
+              if (dist <= 0) {
+                // front
+                if (abs_dist < min_right_front_dist) {
+                  min_right_front_dist = abs_dist;
+                  right_front_car[0] = f_s;
+                  right_front_car[1] = f_d;
+                  right_front_car[2] = f_vx;
+                  right_front_car[3] = f_vy;
+                  right_front_car[4] = f_speed;
+                  right_front_car[5] = abs_dist;
+                }
+              } else {
+                // back
+                if (abs_dist < min_right_back_dist) {
+                  min_right_back_dist = abs_dist;
+                  right_back_car[0] = f_s;
+                  right_back_car[1] = f_d;
+                  right_back_car[2] = f_vx;
+                  right_back_car[3] = f_vy;
+                  right_back_car[4] = f_speed;
+                  right_back_car[5] = abs_dist;
+                }
+              } 
             }
-          }
+            else { 
+              continue; 
+            }
+          }// for sensor fusion
 
-          if (too_close) {
-            ref_vel -= 0.224;
+          double max_cost = std::numeric_limits<double>::max();
+          // cost for keeping lane
+          double cost_keeping_lane = 0;
+          const double DIST_LIMIT_FRONT = 20;
+          const double DIST_LIMIT_SIDE_FRONT = 20;
+          const double DIST_LIMIT_SIDE_BACK = 20;
+          bool close = false;
+          if (front_car[5] < DIST_LIMIT_FRONT) {
+            close = true;
+            cost_keeping_lane = 100;
+          }
+          // cost for changing to left lane
+          double cost_change_left_lane=0;
+          if (our_lane > 0) {
+            if (left_front_car[5] < DIST_LIMIT_SIDE_FRONT || left_back_car[5] < DIST_LIMIT_SIDE_BACK) {
+              cost_change_left_lane = 200;
+            } 
+          } else {
+            cost_change_left_lane = max_cost;
+          }
+          // cost for changing to right lane
+          double cost_change_right_lane = 0;
+          if (our_lane < 2) {
+            if (right_front_car[5] < DIST_LIMIT_SIDE_FRONT || right_back_car[5] < DIST_LIMIT_SIDE_BACK) {
+              cost_change_right_lane = 200;
+            } 
+          } else {
+            cost_change_right_lane = max_cost;
+          }
+          // 0 -> keep lane, 1 -> change left lane, 2 -> change right lane
+          int decision = min_cost_index(cost_keeping_lane, cost_change_left_lane, cost_change_right_lane);
+          double acc = 0.4;
+          if (decision == 1) {
+            lane = our_lane - 1;
+          }
+          if (decision == 2) {
+            lane = our_lane + 1;
+          }
+          if (decision == 0 && close) {
+            ref_vel -= acc;
           }
           else if (ref_vel < 49.5) {
-            ref_vel += 0.224;
+            ref_vel += acc;
           }
 
           vector<double> ptsx;
@@ -178,9 +335,9 @@ int main() {
             ptsy.push_back(ref_y);
           }
 
-          vector<double> next_wp0 = getXY(car_s + 30, get_certer_s_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          vector<double> next_wp1 = getXY(car_s + 60, get_certer_s_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          vector<double> next_wp2 = getXY(car_s + 90, get_certer_s_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp0 = getXY(car_s + 30, get_certer_d_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s + 60, get_certer_d_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s + 90, get_certer_d_from_lane(lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
         
           ptsx.push_back(next_wp0[0]);
           ptsx.push_back(next_wp1[0]);
